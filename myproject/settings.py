1d6a698f08d456a9aa07602926f4f5fb0824f10b
bishimaru <a414510nee@yahoo.co.jp>
2022-12-30 23:07:09 +0900

    success road static/

--- myproject/settings.py
+++ myproject/settings.py
@@ -104,15 +104,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# 本番環境で必要
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+#ローカルで必要
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/',
+]
