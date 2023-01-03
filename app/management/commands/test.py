f0e2253273f625c778a37ca2033964ae83fa106a
bishimaru <a414510nee@yahoo.co.jp>
2023-01-03 18:46:45 +0900

    add chromeDriver

--- app/management/commands/test.py
+++ app/management/commands/test.py
@@ -1,12 +1,25 @@
 from django.core.management.base import BaseCommand
 import requests
 from bs4 import BeautifulSoup
-
+import time
+from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import chromedriver_binary
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        url = "https://www.yahoo.co.jp/"
+        options = ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        url = "https://zenn.dev/"
+        driver = webdriver.Chrome(options=options)
+        driver.get(url)
         r = requests.get(url)
-        soup = BeautifulSoup(r.content, "html.parser")
-        test = soup.find('div', class_='cdcrk1cfotq338-label-1')
+
+        print(driver.title)
+        driver.quit()
+        
         print('hello world')
